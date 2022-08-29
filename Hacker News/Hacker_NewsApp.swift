b504c9189dd5ad8ea8ecd01b12796207c2355e4d
Alexander Altman <qewhouse@gmail.com>
2022-08-29 23:31:18 +0300

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Alexander Altman on 29.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
