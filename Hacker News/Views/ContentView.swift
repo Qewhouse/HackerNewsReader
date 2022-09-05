bf1bd761890167ff0769e145747c772783998795
Alexander Altman <qewhouse@gmail.com>
2022-09-06 00:51:34 +0300

    creating a repo

---
+++ Hacker News/Views/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Hacker News
+//
+//  Created by Alexander Altman on 29.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+                
+            }
+            .navigationTitle("Hacker News")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView().environment(\.colorScheme, .dark)
+            ContentView().environment(\.colorScheme, .light)
+        }
+    }
+}
